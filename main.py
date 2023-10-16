fc5d448219f61daf882501ecc5a760f723823696
ilai <ilai.aloni@gmail.com>
2023-10-15 20:31:14 -0700

    First OOP, scrape, extract, send, store functionality

---
+++ main.py
@@ -0,0 +1,78 @@
+import requests
+import selectorlib
+import smtplib, ssl
+import os
+import time
+import sqlite3
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+
+
+
+class Event:
+    def scrape(self, url):
+        """scrape page source"""
+        response = requests.get(url)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
+
+
+class Email:
+    def send(self, message):
+        host = "smtp.gmail.com"
+        port = 465
+
+        username = "ialoni.py@gmail.com"
+        password = "sstoivtxmzdkwqmr"
+
+        receiver = "ialoni.py@gmail.com"
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print("Email was SENT!")
+
+
+class Database:
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)
+
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
+
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?",
+                       (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
+
+
+if __name__ == "__main__":
+    while True:
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
+        print(extracted)
+        if extracted != "No upcoming tours":
+            database = Database(database_path="data.db")
+            row = database.read(extracted)
+            if not row:
+                database.store(extracted)
+                email = Email()
+                email.send(message="Hey, new event was found!")
+        time.sleep(2)
